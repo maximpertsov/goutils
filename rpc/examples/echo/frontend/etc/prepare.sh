3b117e22c7587bde76940bab2eda1e8044ea4b39
Maxim Pertsov <maxim@viam.com>
2022-12-15 11:51:05 -0500

    generate with protobuf-es only

--- rpc/examples/echo/frontend/etc/prepare.sh
+++ rpc/examples/echo/frontend/etc/prepare.sh
@@ -6,14 +6,6 @@ rm -rf src/gen
 mkdir -p src/gen/proto/rpc/examples
 cp -R ../../../../dist/js/proto/rpc/examples/echo src/gen/proto/rpc/examples
 
-# Ours-ES
-mkdir -p src/gen/proto/rpc_es/examples
-cp -R ../../../../dist/es/proto/rpc/examples/echo src/gen/proto/rpc_es/examples
-
 # Third-Party
 mkdir -p src/gen/google
 cp -R ../../../../dist/js/google/api src/gen/google
-
-# Third-Party-ES
-mkdir -p src/gen/google_es
-cp -R ../../../../dist/es/google/api src/gen/google_es
